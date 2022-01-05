99dc301efe859818f89b101595647f3f26b784c5
Llewellyn Dsouza <lledsouza2209@gmail.com>
2022-01-05 18:13:50 +0530

    Updated: i18 plugin variable

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import HttpApi from 'i18next-http-backend';
+import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Test from './components/Test';
 
 i18next
-  .use(HttpApi)
+  .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
