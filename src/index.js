fb3e7cf3bef2d749df164bf9414f2d4aa92dc418
Llewellyn Dsouza <lledsouza2209@gmail.com>
2022-01-05 16:02:23 +0530

    Added: suspense loader

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
 
-// import reportWebVitals from './reportWebVitals';
+const loadingScreen = (
+  <div
+    style={{
+      display: 'flex',
+      width: '100vw',
+      height: '100vh',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}
+  >
+    <h3>Loading...</h3>
+  </div>
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={loadingScreen}>
+      <App />
+    </Suspense>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
