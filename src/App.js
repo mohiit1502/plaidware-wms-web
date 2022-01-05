3af4fae1c592bb04f891f78a626698eb18d5adaf
Llewellyn Dsouza <lledsouza2209@gmail.com>
2022-01-05 17:42:28 +0530

    Added: i18next logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import HttpApi from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import Test from './components/Test';
+
+i18next
+  .use(HttpApi)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    supportedLngs: ['en' /*, 'fr'*/],
+    fallbackLng: 'en',
+    debug: false,
+    // Options for language detector
+    detection: {
+      order: ['localStorage', 'sessionStorage', 'cookie', 'htmlTag'],
+      caches: ['localStorage'],
+    },
+    // react: { useSuspense: false },
+    backend: {
+      loadPath: '/assets/locales/{{lng}}/translation.json',
+    },
+  });
+
 function App() {
   return (
     <div className="App">
-      <header>
-        Hey there
-      </header>
+      <Test />
     </div>
   );
 }
