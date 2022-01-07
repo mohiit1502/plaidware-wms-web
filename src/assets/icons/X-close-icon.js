25bf141987c61ce6e6e21432efe56ac06ba25594
Llewellyn Dsouza <lledsouza2209@gmail.com>
2022-01-07 22:30:36 +0530

    Update: fixed svgs

--- src/assets/icons/X-close-icon.js
+++ src/assets/icons/X-close-icon.js
@@ -6,10 +6,10 @@ const XIcon = ({ width = 24, height = 24, color = '#ffffff' }) => (
     viewBox="0 0 24 24"
     fill="none"
     stroke={color}
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    class="feather feather-x"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="feather feather-x"
   >
     <line x1="18" y1="6" x2="6" y2="18"></line>
     <line x1="6" y1="6" x2="18" y2="18"></line>
