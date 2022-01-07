b2a908130739ba28bb6191090b5ab0994c635761
Llewellyn Dsouza <lledsouza2209@gmail.com>
2022-01-08 00:15:11 +0530

    Added: Icons

--- src/assets/icons/Hamburger-icon.js
+++ src/assets/icons/Hamburger-icon.js
@@ -1,15 +1,14 @@
-const HamburgerIcon = ({ width = 24, height = 24, color }) => (
+const HamburgerIcon = ({ width = 24, height = 24, color, ...props }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={width}
     height={height}
     viewBox="0 0 24 24"
-    fill="none"
     stroke={color}
     strokeWidth="2"
     strokeLinecap="round"
     strokeLinejoin="round"
-    className="feather feather-menu"
+    {...props}
   >
     <line x1="3" y1="12" x2="21" y2="12"></line>
     <line x1="3" y1="6" x2="21" y2="6"></line>
--- src/assets/icons/X-close-icon.js
+++ src/assets/icons/X-close-icon.js
@@ -1,18 +1,18 @@
-const XIcon = ({ width = 24, height = 24, color = '#ffffff' }) => (
+const XIcon = ({ width = 18, height = 18, color = '#ffffff', ...props }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={width}
     height={height}
-    viewBox="0 0 24 24"
+    viewBox="0 0 14 14"
     fill="none"
     stroke={color}
     strokeWidth="2"
     strokeLinecap="round"
     strokeLinejoin="round"
-    className="feather feather-x"
+    {...props}
   >
-    <line x1="18" y1="6" x2="6" y2="18"></line>
-    <line x1="6" y1="6" x2="18" y2="18"></line>
+    <line x1="13" y1="1" x2="1" y2="13"></line>
+    <line x1="1" y1="1" x2="13" y2="13"></line>
   </svg>
 );
 
