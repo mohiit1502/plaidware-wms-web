5c54936ac10d6f44e4cd0b92963251eb56a12620
Llewellyn D'souza <lledsouza2209@gmail.com>
2022-01-07 17:13:16 +0530

    Added: sidebar open-close

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,9 +1,23 @@
+import XIcon from '../../assets/icons/X-close-icon';
 import styles from './sidebar.module.css';
 
-export default function Sidebar() {
-  return (
-    <div className={styles.navSidebar}>
-      <div className={styles.sidebarBranding}>Plaidware</div>
-    </div>
-  );
+export default function Sidebar({ isMobileScreen, isOpen, setOpen }) {
+  if (!isMobileScreen && isOpen) {
+    return (
+      <div className={styles.navSidebar}>
+        <div className={styles.sidebarBrandingHeader}>
+          <div className={styles.sidebarBranding}>Plaidware</div>
+          <div
+            onClick={() => {
+              setOpen(false);
+            }}
+          >
+            <XIcon />
+          </div>
+        </div>
+        <div className={styles.sidebarNav}></div>
+      </div>
+    );
+  }
+  return null;
 }
--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,15 +1,21 @@
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
+
 import Breadcrumbs from '../../components/breadcrumbs';
 import Sidebar from '../../components/Sidebar';
 import TopBar from '../../components/topbar';
+import useWindowDimensions from '../../hooks/useWindowDimensions';
 import styles from './dashboard.module.css';
 
 export default function Dashboard() {
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+  const { isMobileScreen } = useWindowDimensions();
+
   return (
     <div className={styles.dashboardGrid}>
-      <Sidebar />
+      <Sidebar isMobileScreen={isMobileScreen} isOpen={sidebarOpen} setOpen={setSidebarOpen} />
       <div className={styles.mainContent}>
-        <TopBar />
+        <TopBar isMobileScreen={isMobileScreen} isSidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
         <Breadcrumbs />
         <div className={styles.content}>
           <Outlet />
