940c1e704efb05d5db2eba3fa2df7abd92be0bf6
Llewellyn D'souza <lledsouza2209@gmail.com>
2022-03-08 16:11:55 +0530

    Linted: dropdown

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Box from '@mui/material/Box';
 import PropTypes from 'prop-types';
 import MenuItem from '@mui/material/MenuItem';
